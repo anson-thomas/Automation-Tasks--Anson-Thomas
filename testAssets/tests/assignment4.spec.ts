e9ebe0dda7c57784c1f5f9a83af07242d0350921
anson-thomas <anson.thomas@testingmavens.com>
2026-09-21 14:31:06 +0530

    Addedd screenshot for each test step

--- testAssets/tests/assignment4.spec.ts
+++ testAssets/tests/assignment4.spec.ts
@@ -1,68 +1,66 @@
 import {expect,test} from "../pages/customFixtures"
 import data from "../test-data/data.json"
+import { testStep } from "../../helper/testStep";
 
 test.describe("Day 4 Assignment",  ()=>{
-    test("Verify a user can fill details in the fields", async({homePage})=>
+    test("Verify a user can fill details in the fields", async({ page, homePage })=>
     {
-        await test.step("Verify user is able to launch the webpage",async()=>{
+        await testStep("Verify user is able to launch the webpage", page, async()=>{
             await homePage.launchWebPage();
             await expect(homePage.header).toBeVisible();
         })
-        await test.step("Verify user can enter the first name", async()=>{
+        await testStep("Verify user can enter the first name", page, async()=>{
             await homePage.enterFieldValues(data.fieldName.firstName,data.fieldValue.firstName);
-            await expect(data.fieldValue.firstName).toBeTruthy();
+            await expect(await homePage.getFieldValue(data.fieldName.firstName)).toBe(data.fieldValue.firstName);
         })
-        await test.step("Verify user can enter the last name", async()=>{
+        await testStep("Verify user can enter the last name", page, async()=>{
             await homePage.enterFieldValues(data.fieldName.lastName,data.fieldValue.lastName);
-            await expect(data.fieldValue.lastName).toBeTruthy();
+            await expect(await homePage.getFieldValue(data.fieldName.lastName)).toBe(data.fieldValue.lastName);
         })
-        await test.step("Verify user can enter the email", async()=>{
+        await testStep("Verify user can enter the email", page, async()=>{
             await homePage.enterFieldValues(data.fieldName.email,data.fieldValue.email);
-            await expect(data.fieldValue.email).toBeTruthy();
+            await expect(await homePage.getFieldValue(data.fieldName.email)).toBe(data.fieldValue.email);
         })
-        await test.step("Verify user can enter the password", async()=>{
+        await testStep("Verify user can enter the password", page, async()=>{
             await homePage.enterFieldValues(data.fieldName.password,data.fieldValue.password);
-            await expect(data.fieldValue.password).toBeTruthy();
+            await expect(await homePage.getFieldValue(data.fieldName.password)).toBe(data.fieldValue.password);
         })
-        await test.step("Verify user can enter the repeat password", async()=>{
+        await testStep("Verify user can enter the repeat password", page, async()=>{
             await homePage.enterFieldValues(data.fieldName.repeatPassword,data.fieldValue.repeatPassword);
-            await expect(data.fieldValue.repeatPassword).toBeTruthy();
+            await expect(await homePage.getFieldValue(data.fieldName.repeatPassword)).toBe(data.fieldValue.repeatPassword);
         })
-        await test.step("Print all input values from UI", async () => {
-
-    console.log("First Name:", await homePage.getFieldValue("First Name"));
-    console.log("Last Name:", await homePage.getFieldValue("Last Name"));
-    console.log("Email:", await homePage.getFieldValue("Email"));
-    console.log("Password:", await homePage.getFieldValue("Password"));
-    console.log("Repeat Password:", await homePage.getFieldValue("Repeat Password"));
-
+        await testStep("Print all input values from UI", page, async () => {
+        console.log("First Name:", await homePage.getFieldValue("First Name"));
+        console.log("Last Name:", await homePage.getFieldValue("Last Name"));
+        console.log("Email:", await homePage.getFieldValue("Email"));
+        console.log("Password:", await homePage.getFieldValue("Password"));
+        console.log("Repeat Password:", await homePage.getFieldValue("Repeat Password"));
+        });
 });
-});
-    test("Verify you can print details as you need from the table",async({homePage})=>
+
+    test("Verify you can print details as you need from the table",async({ page, homePage })=>
     {
-        await test.step("Verify user is able to launch the webpage",async()=>{
+        await testStep("Verify user is able to launch the webpage", page, async()=>{
             await homePage.launchWebPage();
             await expect(homePage.header).toBeVisible();
         })
         const details = await homePage.getTableDetails();        
-        await test.step("Print Contact, Country and Salary", async () => {
+        await testStep("Print Contact, Country and Salary", page, async () => {
         for (const row of details) {
         console.log(
             `Contact : ${row.Contact}; Country: ${row.Country}; Salary: ${row.Salary}`
         );
     }
         });
-
-        await test.step("Print salary greater than and less than 5000", async () => {
+    
+        await testStep("Print salary greater than and less than 5000", page, async () => {
         console.log("\n Objects with salary greater than and less than 5000 \n");    
         for (const row of details) {
             if (row.Salary > 5000 || row.Salary < 5000) {
                 console.log(row);
             }
         }
+        });
     });
-    }
-)
-
 });
 
