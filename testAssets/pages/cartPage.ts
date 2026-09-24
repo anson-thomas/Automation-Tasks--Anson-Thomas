619d681ea39135b5327fd4043028bdbd143fa8ab
anson-thomas <anson.thomas@testingmavens.com>
2026-09-24 17:13:25 +0530

    Corrected the locators and removed unwanted methods

--- testAssets/pages/cartPage.ts
+++ testAssets/pages/cartPage.ts
@@ -3,50 +3,35 @@ import Actions from "../../helper/actions";
 
 
 export default class CartPage {
-     actions: Actions;   
-     cartIcon: Locator;
-     cartCount: Locator;
-     cartHeading: Locator;
-     productRows: Locator;
-     productName: Locator;
-     unitPrice: Locator;
-     quantity: Locator;
-     subTotal: Locator;
-     total: Locator;
-     cartSubTotal: Locator;
-     resetCartButton: Locator;
-     emptyCartmsg: Locator;
+        actions: Actions;
+        cartIcon: Locator;
+        cartCount: Locator;
+        cartHeading: Locator;
+        productName: Locator;
+        unitPrice: Locator;
+        quantity: Locator;
+        cartSubTotal: Locator;
 
     constructor(public page: Page) {
         this.actions = new Actions();
-        this.cartIcon = this.page.locator("//a[@href='/cart']");
-        this.cartCount = this.page.locator("//a[@href='/cart']//span");
-        this.cartHeading = this.page.locator("//h1[normalize-space()='Cart']");
-        this.productRows = this.page.locator("//div[contains(@class,'grid') and .//h1]");
-        this.productName = this.page.locator("//h1[@class='font-titleFont font-semibold']");
-        this.unitPrice = this.page.locator("//div[@class='flex w-1/3 items-center text-lg font-semibold']");
-        this.quantity = this.page.locator("//div[@class='w-1/3 flex items-center gap-6 text-lg']//p");
-        this.subTotal = this.page.locator("//div[@class='w-1/3 flex items-center font-titleFont font-bold text-lg']//p");
-        this.total = this.page.locator("//p[contains(normalize-space(), 'Total')]/span");
-        this.cartSubTotal = this.page.locator("//p[contains(normalize-space(), 'Subtotal')]/span");
-        this.resetCartButton= this.page.locator("//button[normalize-space()='Reset cart']")
-        this.emptyCartmsg = this.page.locator("//h1[text()='Your Cart feels lonely.']")
+        this.cartIcon = this.page.locator("//div[@class='relative']/following::a[@href='/cart']");
+        this.cartCount = this.page.locator("//a[@href='/cart']/descendant::span");
+        this.cartHeading = this.page.locator("//div[contains(@class,'auto px-4')]/descendant::h1[1]");
+        this.productName = this.page.locator("//div[contains(@class,'border py-2')]/descendant::h1");
+        this.unitPrice = this.page.locator("//div[contains(@class,'gap-0')]/descendant::div[1]");
+        this.quantity = this.page.locator("//div[contains(@class,'gap-0')]/descendant::div[2]/p");
+        this.cartSubTotal = this.page.locator("//p[contains(normalize-space(), 'Subtotal')]/descendant::span");
     }
+    
     async openCart() {
     await this.actions.click(this.cartIcon);
     await expect(this.cartHeading).toBeVisible();
     }
-async getCartCount(): Promise<number> {
-    await expect(this.cartCount).toHaveText("2");
-
-    const count = await this.cartCount.innerText();
-
-    return Number(count);
-}
-    async getProductCount(): Promise<number> {
-        return await this.productRows.count();
+    async getCartCount(): Promise<number> {
+        const count = await this.cartCount.innerText();
+        return Number(count);
     }
-    async getProductNames(): Promise<string[]> {
+        async getProductNames(): Promise<string[]> {
         return await this.productName.allTextContents();
     }
     async getUnitPrices(): Promise<number[]> {
@@ -58,25 +43,9 @@ async getCartCount(): Promise<number> {
         const quantities = await this.quantity.allTextContents();
         return quantities.map((quantity) => Number(quantity.trim())
         );
-    }
-    async getSubTotals(): Promise<number[]> {
-        const subTotals = await this.subTotal.allTextContents();
-        return subTotals.map((subTotal) => Number(subTotal.replace("$", "").trim())
-        );
-    }
-    async getTotal(): Promise<number> {
-        const total = await this.total.innerText();
-        return Number(total.replace("$", "").trim());
-    }
-    async verifyProductInCart(productName: string): Promise<boolean> {
-    const product = this.productRows.filter({hasText: productName});
-    return await product.count() > 0;
-    }
+    }    
     async getCartSubtotal(): Promise<number> {
     const subtotal = await this.cartSubTotal.innerText();
     return Number(subtotal.replace("$", "").trim());
-    }
-    async resetCart() {
-    await this.actions.click(this.resetCartButton);
     }
 }
--- testAssets/tests/assignment6.spec.ts
+++ testAssets/tests/assignment6.spec.ts
@@ -3,209 +3,106 @@ import { testStep } from "../../helper/utility";
 import fs from "fs";
 
 test.describe("New Arrivals Assignment", () => {
-
     test("Identify, sort and capture minimum and maximum priced products",
         async ({ page, homePage,cartPage }) => {
-
+            
+            await testStep("Verify user is able to launch the webpage",page,async()=>{
             await homePage.launchWebApp();
-
-            await expect(homePage.header).toBeVisible();
-
-            await homePage.newArrivalsHeading.waitFor({
-                state: "visible"
+            await homePage.waitForNewArrivalImages();
+            await expect(homePage.newArrivalsSection).toBeVisible();              
             });
 
-            await homePage.waitForNewArrivalImages();
-
-            // Task 1: Get all New Arrivals products and prices
-
-            const products =
-                await homePage.getNewArrivalProducts();
-
-            console.log("New Arrivals:", products);
-
-            // Task 2: Sort products from minimum to maximum price
-
-            products.sort(
-                (a, b) => a.price - b.price
+            await testStep("Verify all new arrival products and their prices can be identified",page,async()=>{
+            const products = await homePage.getNewArrivalProducts();
+            console.log("New Arrivals:", products);                
+            },
+            homePage.newArrivalsSection
             );
 
-            console.log("Sorted Products:", products);
-
+            const products = await homePage.getNewArrivalProducts(); 
+            products.sort((a, b) => a.price - b.price);
             const minProduct = products[0];
-            const maxProduct =
-                products[products.length - 1];
+            const maxProduct = products[products.length - 1];
 
+            await testStep("Verify all new arrival products are sorted from minimum to maximum price and stored the minimum and maximum product details in JSON.",page,async()=>{
+            console.log("Sorted Products:", products);
             console.log("Minimum Product:", minProduct);
             console.log("Maximum Product:", maxProduct);
+            const productData = {minimumProduct: minProduct,maximumProduct: maxProduct};
+            fs.writeFileSync("testAssets/test-data/productData.json",JSON.stringify(productData, null, 4));
+            })
 
-            // Store minimum and maximum product details in JSON
-
-            const productData = {
-                minimumProduct: minProduct,
-                maximumProduct: maxProduct
-            };
-
-            fs.writeFileSync(
-                "testAssets/test-data/productData.json",
-                JSON.stringify(productData, null, 4)
+            await testStep("Minimum priced product",page,async () => {
+                const minimumProduct =homePage.getNewArrivalProductCard(minProduct.name);
+                await minimumProduct.scrollIntoViewIfNeeded();
+                }                
             );
 
-            // Task 3: Locate and screenshot minimum priced product
-
-            const minimumProduct =
-                homePage.getNewArrivalProductCard(
-                    minProduct.name
-                );
-
-            await testStep(
-                "Minimum priced product",
-                page,
-                async () => {
-                    await minimumProduct.scrollIntoViewIfNeeded();
-                },
-                minimumProduct
+            await testStep("Maximum priced product",page,async () => {
+                const maximumProduct = homePage.getNewArrivalProductCard(maxProduct.name);
+                await maximumProduct.scrollIntoViewIfNeeded();
+                }
             );
 
-            // Locate and screenshot maximum priced product
-
-            const maximumProduct =
-                homePage.getNewArrivalProductCard(
-                    maxProduct.name
-                );
-
-            await testStep(
-                "Maximum priced product",
-                page,
-                async () => {
-                    await maximumProduct.scrollIntoViewIfNeeded();
-                },
-                maximumProduct
+            await testStep("Add minimum priced product to cart",page,async () => {
+                await homePage.addProductToCart(minProduct.name);
+                await expect(cartPage.cartCount).toHaveText("1");
+            }
             );
-await testStep(
-    "Add minimum priced product to cart",
-    page,
-    async () => {
-        await homePage.addProductToCart(
-            minProduct.name
-        );
 
-        await expect(cartPage.cartCount).toHaveText("1");
-    }
-);
+            await testStep("Add maximum priced product to cart",page,async () => {
+                await homePage.addProductToCart(maxProduct.name);
+                await expect(cartPage.cartCount).toHaveText("2");
+                }
+            );
 
-await testStep(
-    "Add maximum priced product to cart",
-    page,
-    async () => {
-        await homePage.addProductToCart(
-            maxProduct.name
-        );
+            await testStep("Validate cart item count",page,async () => {
+                
+                await cartPage.actions.highlight(cartPage.cartCount);
+                await homePage.openCart();
+                const cartCount = await cartPage.getCartCount();
+                await expect(cartCount).toBe(2);
+                }
+            );
+            
+            await testStep("Validate product names",page,async () => {
+                    const productNames = cartPage.productName;
+                    for (let i = 0; i < await productNames.count(); i++) {
+                        await cartPage.actions.highlight(productNames.nth(i)
+                        );
+                    }
+                    const cartProductNames =await cartPage.getProductNames();
+                    await expect(cartProductNames).toContain(minProduct.name);
+                    await expect(cartProductNames).toContain(maxProduct.name);
+                }
+            );
 
-        await expect(cartPage.cartCount).toHaveText("2");
-    }
-);
-await homePage.openCart();
-await testStep(
-    "Validate cart item count",
-    page,
-    async () => {
-        await cartPage.actions.highlight(
-            cartPage.cartCount
-        );
-
-        const cartCount =
-            await cartPage.getCartCount();
-
-        await expect(cartCount).toBe(2);
-    }
-);
-
-await testStep(
-    "Validate product names",
-    page,
-    async () => {
-        const productNames = cartPage.productName;
-
-        for (let i = 0; i < await productNames.count(); i++) {
-            await cartPage.actions.highlight(
-                productNames.nth(i)
+            await testStep("Validate product prices",page,async () => {
+                    const unitPrices = cartPage.unitPrice;
+                    for (let i = 0; i < await unitPrices.count(); i++) {
+                        await cartPage.actions.highlight(unitPrices.nth(i));
+                    }
+                    const cartPrices =await cartPage.getUnitPrices();
+                    await expect(cartPrices).toContain(minProduct.price);
+                    await expect(cartPrices).toContain(maxProduct.price);
+                }
+            );
+            await testStep("Validate product quantities",page,async () => {
+                    const quantities = cartPage.quantity;
+                    for (let i = 0; i < await quantities.count(); i++) {
+                        await cartPage.actions.highlight(quantities.nth(i));
+                    }
+                    const cartQuantities =await cartPage.getQuantities();
+                    await expect(cartQuantities).toEqual([1, 1]);
+                }
+            );
+            await testStep("Validate cart subtotal",page,async () => {
+                    await cartPage.actions.highlight(cartPage.cartSubTotal);
+                    const expectedTotal = minProduct.price + maxProduct.price;
+                    const cartTotal = await cartPage.getCartSubtotal();
+                    await expect(cartTotal).toBe(expectedTotal);
+                }
             );
         }
-
-        const cartProductNames =
-            await cartPage.getProductNames();
-
-        await expect(cartProductNames).toContain(
-            minProduct.name
-        );
-
-        await expect(cartProductNames).toContain(
-            maxProduct.name
-        );
-    }
-);
-
-await testStep(
-    "Validate product prices",
-    page,
-    async () => {
-        const unitPrices = cartPage.unitPrice;
-
-        for (let i = 0; i < await unitPrices.count(); i++) {
-            await cartPage.actions.highlight(
-                unitPrices.nth(i)
-            );
-        }
-
-        const cartPrices =
-            await cartPage.getUnitPrices();
-
-        await expect(cartPrices).toContain(
-            minProduct.price
-        );
-
-        await expect(cartPrices).toContain(
-            maxProduct.price
-        );
-    }
-);
-await testStep(
-    "Validate product quantities",
-    page,
-    async () => {
-        const quantities = cartPage.quantity;
-
-        for (let i = 0; i < await quantities.count(); i++) {
-            await cartPage.actions.highlight(
-                quantities.nth(i)
-            );
-        }
-
-        const cartQuantities =
-            await cartPage.getQuantities();
-
-        await expect(cartQuantities).toEqual([1, 1]);
-    }
-);
-await testStep(
-    "Validate cart subtotal",
-    page,
-    async () => {
-        await cartPage.actions.highlight(
-            cartPage.cartSubTotal
-        );
-
-        const expectedTotal =
-            minProduct.price + maxProduct.price;
-
-        const cartTotal =
-            await cartPage.getCartSubtotal();
-
-        await expect(cartTotal).toBe(
-            expectedTotal
-        );
-    }
-);}
     );
 });
